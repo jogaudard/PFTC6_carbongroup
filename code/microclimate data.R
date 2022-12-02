315901b8ca032d8fb9417fed1d8f3b51b4464fe1
Hilary Rose Dawson <hrosedawson@gmail.com>
2022-12-02 07:59:59 -0800

    read in files

--- code/microclimate data.R
+++ code/microclimate data.R
@@ -1,5 +1,6 @@
 # Fetch the data from OSF PFTC6 ----
 library(dataDownloader)
+library(tidyverse)
 
 # Download microclimate data
 get_file(node = "fcbw4",
@@ -16,3 +17,15 @@ get_file(node = "fcbw4",
 #Unzip microclimate data
 unzip("raw_data/PFTC6_microclimate_2022.zip", exdir = "raw_data/microclimate")
 file.remove("raw_data/PFTC6_microclimate_2022.zip") #let's free some space
+
+# Climate data ----
+## Read in files ----
+# Make file list
+files <- dir(path = "raw_data/microclimate", pattern = "^data.*\\.csv$", full.names = TRUE, recursive = TRUE)
+
+# Read in data
+temp <- map_df(set_names(files), function(file) {
+    file %>% 
+    set_names() %>% 
+    map_df(~ read_delim(file = file, col_names = FALSE, delim = ";"))
+  }, .id = "File")
