245a04af00b560d89799593e173baa8a9d316f99
Hilary Rose Dawson <hrosedawson@gmail.com>
2022-12-02 07:54:06 -0800

    unzipped file

--- code/microclimate data.R
+++ code/microclimate data.R
@@ -12,3 +12,7 @@ get_file(node = "fcbw4",
          file = "PFTC6_microclimate_metadata.csv",
          path = "raw_data",
          remote_path = "raw_data/c_flux_raw_data")
+
+#Unzip microclimate data
+unzip("raw_data/PFTC6_microclimate_2022.zip", exdir = "raw_data/microclimate")
+file.remove("raw_data/PFTC6_microclimate_2022.zip") #let's free some space
