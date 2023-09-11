9e1ef10df76ebe10ea53e72e0c7f1b64e6c4bca2
jogaudard <joseph.gaudard@pm.me>
2023-09-11 16:56:01 +0200

    trying some more visualization and correcting errors in microclimate cleaning script

--- code/microclimate_data.R
+++ code/microclimate_data.R
@@ -19,7 +19,7 @@ get_file(node = "fcbw4",
 
 #Download microclimate metadata
 get_file(node = "fcbw4",
-         file = "PFTC6_microclimate_metadata.csv",
+         file = "PFTC6_microclimate_metadata_all.csv",
          path = "raw_data",
          remote_path = "raw_data/c_flux_raw_data")
 
