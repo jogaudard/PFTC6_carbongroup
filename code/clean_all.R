584cc0b643210630b107321076dc7ef51da685d3
jogaudard <joseph.gaudard@pm.me>
2023-09-14 14:40:48 +0200

    added destSiteID and origSiteID to flux and microclimate clean datasets

--- code/clean_all.R
+++ code/clean_all.R
@@ -15,6 +15,7 @@ my_packages <- c("dataDownloader",
 lapply(my_packages, library, character.only = TRUE) 
 
 sources <- list("code/functions.R",
+                "code/metaturf.R",
                 "code/cleaning_vikesland.R",
                 "code/cleaning_liahovden.R",
                 "code/cleaning_joasete.R",
@@ -36,32 +37,33 @@ sapply(sources, source)
 
 # creating full dataset ---------------------------------------------------
 
-cflux_vikesland_corrected <- cflux_vikesland_corrected %>% 
-  mutate(
-    site = "vikesland"
-  )
-
-cflux_liahovden_corrected <- cflux_liahovden_corrected %>% 
-  mutate(
-    site = "liahovden"
-  )
-
-cflux_joasete_corrected <- cflux_joasete_corrected %>% 
-  mutate(
-    site = "joasete"
-  )
-
-cflux_hogsete_corrected <- cflux_hogsete_corrected %>% 
-  mutate(
-    site = "hogsete"
-  )
+# cflux_vikesland_corrected <- cflux_vikesland_corrected %>% 
+#   mutate(
+#     site = "Vik"
+#   )
+# 
+# cflux_liahovden_corrected <- cflux_liahovden_corrected %>% 
+#   mutate(
+#     site = "Lia"
+#   )
+# 
+# cflux_joasete_corrected <- cflux_joasete_corrected %>% 
+#   mutate(
+#     site = "Joa"
+#   )
+# 
+# cflux_hogsete_corrected <- cflux_hogsete_corrected %>% 
+#   mutate(
+#     site = "Hog"
+#   )
 
 cflux_all_clean <- bind_rows(
   cflux_vikesland_corrected,
   cflux_liahovden_corrected,
   cflux_joasete_corrected,
   cflux_hogsete_corrected
-)
+) %>% 
+  left_join(metaturf)
 
 write_csv(cflux_all_clean, "clean_data/PFTC6_24h_cflux_allsites_2022.csv")
 
