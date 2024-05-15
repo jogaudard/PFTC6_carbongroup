872317b951250416f7aa233f79debf6141c2fa19
hrdawson <hrosedawson@gmail.com>
2024-05-15 13:16:53 -0700

    Separating function into its own script

--- code/data_dic/download_read_clean_data.R
+++ code/data_dic/download_read_clean_data.R
@@ -3,6 +3,22 @@ library(dataDownloader)
 source("code/metaturf.R")
 
 
+
+
+# All CFlux data ----
+get_file(node = "fcbw4",
+         file = "PFTC6_24h_cflux_allsites_2022.csv",
+         path = "clean_data",
+         remote_path = "v. c_flux_data")
+
+cflux <- read_csv("clean_data/PFTC6_24h_cflux_allsites_2022.csv")
+
+# Microclimate data ----
+get_file(node = "fcbw4",
+         file = "PFTC6_clean_microclimate_2022.csv",
+         path = "clean_data",
+         remote_path = "vii. microclimate_data")
+
 # meta turfID -------------------------------------------------------------
 
 # get_file(node = "pk4bg",
@@ -13,45 +29,36 @@ source("code/metaturf.R")
 # metadata <- read_csv("clean_data/Three-D_metaturfID.csv") %>%
 #   select(warming, turfID)
 
-# All CFlux data ----
-get_file(node = "fcbw4",
-         file = "PFTC6_24h_cflux_allsites_2022.csv",
-         path = "clean_data",
-         remote_path = "v. c_flux_data")
-
-cflux <- read_csv("clean_data/PFTC6_24h_cflux_allsites_2022.csv")
-
-
 # CFlux data -------------------------------------------------------------
-
-get_file(node = "fcbw4",
-         file = "PFTC6_24h-cflux_vikesland_2022.csv",
-         path = "clean_data",
-         remote_path = "c_flux_data")
-
-cflux_vikesland <- read_csv("clean_data/PFTC6_24h-cflux_vikesland_2022.csv", col_types = "ffdddTtd")
-
-get_file(node = "fcbw4",
-         file = "PFTC6_24h-cflux_hogsete_2022.csv",
-         path = "clean_data",
-         remote_path = "c_flux_data")
-
-cflux_hogsete <- read_csv("clean_data/PFTC6_24h-cflux_hogsete_2022.csv", col_types = "ffdddTtd")
-
-get_file(node = "fcbw4",
-         file = "PFTC6_24h-cflux_liahovden_2022.csv",
-         path = "clean_data",
-         remote_path = "c_flux_data")
-
-cflux_liahovden <- read_csv("clean_data/PFTC6_24h-cflux_liahovden_2022.csv", col_types = "ffdddTtd")
-
-get_file(node = "fcbw4",
-         file = "PFTC6_24h-cflux_joasete_2022.csv",
-         path = "clean_data",
-         remote_path = "c_flux_data")
-
-cflux_joasete <- read_csv("clean_data/PFTC6_24h-cflux_joasete_2022.csv", col_types = "ffdddTtd")
-
-#all fluxes together
-cflux <- bind_rows(cflux_vikesland, cflux_hogsete, cflux_liahovden, cflux_joasete) %>%
-  right_join(metaturf)
+#
+# get_file(node = "fcbw4",
+#          file = "PFTC6_24h-cflux_vikesland_2022.csv",
+#          path = "clean_data",
+#          remote_path = "c_flux_data")
+#
+# cflux_vikesland <- read_csv("clean_data/PFTC6_24h-cflux_vikesland_2022.csv", col_types = "ffdddTtd")
+#
+# get_file(node = "fcbw4",
+#          file = "PFTC6_24h-cflux_hogsete_2022.csv",
+#          path = "clean_data",
+#          remote_path = "c_flux_data")
+#
+# cflux_hogsete <- read_csv("clean_data/PFTC6_24h-cflux_hogsete_2022.csv", col_types = "ffdddTtd")
+#
+# get_file(node = "fcbw4",
+#          file = "PFTC6_24h-cflux_liahovden_2022.csv",
+#          path = "clean_data",
+#          remote_path = "c_flux_data")
+#
+# cflux_liahovden <- read_csv("clean_data/PFTC6_24h-cflux_liahovden_2022.csv", col_types = "ffdddTtd")
+#
+# get_file(node = "fcbw4",
+#          file = "PFTC6_24h-cflux_joasete_2022.csv",
+#          path = "clean_data",
+#          remote_path = "c_flux_data")
+#
+# cflux_joasete <- read_csv("clean_data/PFTC6_24h-cflux_joasete_2022.csv", col_types = "ffdddTtd")
+#
+# #all fluxes together
+# cflux <- bind_rows(cflux_vikesland, cflux_hogsete, cflux_liahovden, cflux_joasete) %>%
+#   right_join(metaturf)
