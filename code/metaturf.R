2b6af745470f17093dfe47f7c500ddeb6d4c4180
hrdawson <hrosedawson@gmail.com>
2024-05-15 09:00:06 -0700

    Updated site names

--- code/metaturf.R
+++ code/metaturf.R
@@ -8,31 +8,31 @@ get_file(node = "pk4bg",
 
 meta_seedclim <- tibble(
   turfID = c("TTC 101", "TTC 110", "TTC 115", "TTC 146", "TTC 140", "TTC 141"),
-  origin = c("hogsete", "hogsete", "hogsete", "vikesland", "vikesland", "vikesland"),
-  destination = c("hogsete", "hogsete", "hogsete", "vikesland", "vikesland", "vikesland"),
+  origin = c("Hogsete", "Hogsete", "Hogsete", "Vikesland", "Vikesland", "Vikesland"),
+  destination = c("Hogsete", "Hogsete", "Hogsete", "Vikesland", "Vikesland", "Vikesland"),
   warming = "ambient"
 )
 
-metaturf <- read_csv("raw_data/Three-D_metaturfID.csv") %>% 
-  select(warming, origSiteID, turfID, destSiteID) %>% 
+metaturf <- read_csv("raw_data/Three-D_metaturfID.csv") %>%
+  select(warming, origSiteID, turfID, destSiteID) %>%
   rename(
     origin = origSiteID,
     destination = destSiteID
-  ) %>% 
+  ) %>%
   mutate(
     origin = str_replace_all(
       origin,
-      c("Lia" = "liahovden" , "Joa" = "joasete", "Vik" = "vikesland")
+      c("Lia" = "Liahovden" , "Joa" = "Hoasete", "Vik" = "Vikesland")
     ),
     destination = str_replace_all(
       destination,
-      c("Lia" = "liahovden" , "Joa" = "joasete", "Vik" = "vikesland")
+      c("Lia" = "Liahovden" , "Joa" = "Joasete", "Vik" = "Vikesland")
     ),
     warming = str_replace_all(
       warming,
       c("W" = "transplant", "A" = "ambient")
     )
-  ) %>% 
+  ) %>%
   bind_rows(meta_seedclim)
 
 
