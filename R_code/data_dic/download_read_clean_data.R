973b57f3c51a539cf1eb3d51f08d515e14c7ea59
hrdawson <hrosedawson@gmail.com>
2024-05-16 07:02:52 -0700

    Updated file path for clean carbon data

--- R_code/data_dic/download_read_clean_data.R
+++ R_code/data_dic/download_read_clean_data.R
@@ -3,11 +3,11 @@ library(dataDownloader)
 
 # All CFlux data ----
 get_file(node = "fcbw4",
-         file = "PFTC6_24h_cflux_allsites_2022.csv",
+         file = "PFTC6_clean_cflux_2022.csv",
          path = "clean_data",
          remote_path = "v. c_flux_data")
 
-cflux <- read_csv("clean_data/PFTC6_24h_cflux_allsites_2022.csv")
+cflux <- read_csv("clean_data/PFTC6_clean_cflux_2022.csv")
 
 # Microclimate data ----
 get_file(node = "fcbw4",
